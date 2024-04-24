4a5ea3ef6327b13fabb644255bc42b46157e6a85
rashadaghayev85 <rashadra@code.edu.az>
2024-04-24 16:52:14 +0400

    created file

---
+++ Task-Javascript/assets/js/script.js
@@ -0,0 +1,248 @@
+"use stict"
+// let n=21;
+// if(n%21==0){
+// console.log("bolunur");
+// }
+// else{
+//     console.log("bolunmur");
+// }
+
+// let n=21;
+// let m=24;
+// let result=1
+// if(n%2==0&&m%2==0){
+//     result=m*n
+//  console.log(result)
+// }
+// else{
+//     console.log("edelerin her ikisi cut deyil")
+// }
+// let sum=0;
+// for(i=n;i<=m;i++){
+
+//     if(i%2!=0){
+//         sum+=i
+//     }
+
+// }
+// console.log(sum)
+
+
+// let count=0;
+// for(i=n;i<=m;i++){
+
+//     if(i%2!=0){
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+
+// for(i=n;i<=m;i++){
+//     console.log(i)
+// }
+
+
+//let arr=[1,2,3,4,5]
+// let count=0;
+// for(i=0;i<=arr.length;i++){
+
+//     if(i%2==0){
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+// let response=1;
+// for(i=0;i<=arr.length;i++){
+
+//     if(1<i&&i<20){
+//        response*=i
+//     }
+
+// }
+// console.log(response);
+
+// let a=300;
+
+// let count=0;
+
+// if(a==1||a==2){
+//     console.log("ne sade ne murekkeb")
+// }
+
+// for(i=1;i<=a;i++){
+  
+//  if(a%i==0){
+//    count++
+//  }
+// }
+// if(count>2){
+//     console.log("murekkebdir")
+// }
+// else{
+//     console.log("murekkeb deyil")
+// }
+
+
+
+/////////////////////////////////////////////////////////////////#Practice
+
+// console.log("salaamm")
+//  var address="Ehmedli";
+//  var address="gence";
+// var isMarried=true;
+// let phone="4378029820";
+// //let phone="480239"
+// const email="test@gmail.com";
+// email="teste3809"//runtime error
+//console.log(100+100)
+
+// surname="test"//error
+// let a=5;
+// let b=10;
+// let result=a+b;
+// //a++
+// //a--
+// console.log(result );
+// let isMarried=true;
+
+// if(isMarried){
+// console.log("evlidir");
+// }
+// else{
+//     console.log("subaydir");
+// }
+
+// let number=100;
+// if(number>50&&number<150){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no"); 
+// }
+// let weekDay="Wednesday";
+// switch(weekDay){
+//     case"Tuesday":
+//     console.log("Tuesday")
+//     break;
+//     case"Wednesday":
+//     console.log("Wednesday")
+//     break;
+//     default:
+//         console.log("not select");
+//         break;
+// }
+
+// alert("Welcomee")
+
+// let result=confirm("Press a button!");
+// console.log(result);
+
+// let person=prompt("ewiohcebcb","haryy");
+// console.log(person);
+// if(person==null){
+//     console.log("operation canceled");
+// }
+// else{
+//     console.log(person);
+// }
+
+// let a=5;
+// while(a<8){
+//     console.log(a);
+//     a++;
+// }
+
+// for(let i=0; i<50; i++){
+//     console.log(i);
+// }
+// let a=5;
+// let b=a**2;
+// console.log(b);
+// let num1=100;
+// let num2="100";
+// console.log(num1==num2);//true
+// console.log(num1===num2);//false
+
+// let datas=[1,2,3,"salam",true];
+
+// let isActive=true;
+// console.log(typeof isActive=='boolean')
+//console.log(datas[3]);
+// for(let i=0; i<datas.length;i++){
+//     console.log(datas[i]);
+// }
+// for(let i=0; i<datas.length;i++){
+//     if(!isNaN(datas[i])&& typeof datas[i]!='boolean'){
+//         console.log(datas[i]);
+//     }
+// }
+// for(let i=0; i<datas.length;i++){
+//         if(typeof datas[i=='number']){
+//             console.log(datas[i]);
+//         }
+//     }
+
+// let elem=50;
+// let result=elem>100? "yes":"no";
+// console.log(result);
+
+// let b;
+// let a=null;
+// console.log(a+b);
+
+// let numbers=[20,30,2,2,4,5];
+
+// let sum=0;
+// for(let i=0;i<numbers.length;i++){
+//     sum+=numbers[i];
+// }
+// console.log(sum);
+
+// let isTrue=true;
+// if(isTrue){
+// let address="Tiblisi";
+
+// if(isTrue){
+// let surname="Fexriyye"
+// }
+//     // var surname="Fexriyye";
+//     // let surname="Fexriyye";//error
+// }
+// console.log(surname);
+
+
+
+
+
+
+// const data="Product1";
+
+// for(let i=0;i<products.length;i++){
+//     console.log(products[i])
+// }
+
+// const name="test";
+// nam="dhb"//error
+// const datas=[1,2,3,4];
+// const datas2=[1,2,3,4];
+// datas=datas2//error
+// datas[2]=100;//not error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
